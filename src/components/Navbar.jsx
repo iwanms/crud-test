543b22daa7cf04d196ab2ac3104b9b1ca2c9efe8
iwanms <iwanmaolanasidik1@gmail.com>
2024-12-02 21:26:14 +0700

    ubah path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
+  const rootPath = router.pathname === "/";
   return (
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-      <Link href={"/"} className="text-white font-bold text-2xl">
+      <Link href="/" className="text-white font-bold text-2xl">
         Quotes
       </Link>
       <Link href={"/addQuote"} className="bg-white px-3 py-2">
